cad4272cf0a64ed2270d092c3d3f9dd5947caa1e
RAKE-237 <61335818+RAKE-237@users.noreply.github.com>
2021-05-19 16:54:00 -0400

    Add files via upload

--- src/Veiculo.java
+++ src/Veiculo.java
@@ -1,4 +1,67 @@
+package Estoque;
 
 public abstract class Veiculo {
+	
+	private String ID;
+	private int quantidade;
+	private String modelo;
+	private int ano;
+	private float preco;
+	
+	
+	public Veiculo(String iD, int quantidade, String modelo, int ano, float preco) {
+		setID(iD);
+		setQuantidade(quantidade);
+		setModelo(modelo);
+		setAno(ano);
+		setPreco(preco);
+	}
+	
+	
+	public String getID() {
+		return ID;
+	}
+	private void setID(String iD) {
+		ID = iD;
+	}
+	public int getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	private void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public int getAno() {
+		return ano;
+	}
+	private void setAno(int ano) {
+		this.ano = ano;
+	}
+	public float getPreco() {
+		return preco;
+	}
+	public void setPreco(float preco) {
+		this.preco = preco;
+	}
 
+	public void retirarVeiculo() {
+		quantidade--;
+	}
+	
+	public void adicionarVeiculo() {
+		quantidade++;
+	}
+	
+	public void aumentarPreco() {
+		preco++;
+	}
+	
+	public void diminuirPreco() {
+		preco--;
+	}
 }
