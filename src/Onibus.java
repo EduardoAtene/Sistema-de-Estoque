c2ffae47dd798b19573b6e5673efcee49e3d5975
EduardoAtene <eduardoatenesilvae@gmail.com>
2021-05-19 21:42:49 -0300

    Fazendo alterações

--- src/Onibus.java
+++ src/Onibus.java
@@ -1,24 +1,3 @@
 public class Onibus extends Veiculo {
-	private static int onibusVendidos;
-	private static int totalOnibus;
-	private int acento;
-	
-	public Onibus(String iD, int quantidade, String modelo, int ano, float preco,int acento) {
-		super(iD, quantidade, modelo, ano, preco);
-		this.acento = acento;
-		// TODO Auto-generated constructor stub
-	}	
-	
-	//Getters
-	public static int getOnibusVendidos() {
-		return onibusVendidos;
-	}
-	public static int getTotalOnibus() {
-		return totalOnibus;
-	}
-	public int getAcento() {
-		return acento;
-	}
-
 	
 }
--- src/Veiculo.java
+++ src/Veiculo.java
@@ -1,65 +1,37 @@
 public abstract class Veiculo {
 	
 	private String ID;
-	private int quantidade;
 	private String modelo;
 	private int ano;
 	private float preco;
+	protected int vaga;	
 	
-	
-	public Veiculo(String iD, int quantidade, String modelo, int ano, float preco) {
-		setID(iD);
-		setQuantidade(quantidade);
-		setModelo(modelo);
-		setAno(ano);
-		setPreco(preco);
+	public Veiculo(String ID, String modelo, int ano, float preco) {
+		this.ID = ID;
+		this.modelo = modelo;
+		this.ano = ano;
+		this.preco = preco;
 	}
-	
-	
+		
+	public int getVaga() {
+		return vaga;
+	}
+
+	public abstract void setVaga();
+
+
 	public String getID() {
 		return ID;
 	}
-	private void setID(String iD) {
-		ID = iD;
-	}
-	public int getQuantidade() {
-		return quantidade;
-	}
-	public void setQuantidade(int quantidade) {
-		this.quantidade = quantidade;
-	}
+	
 	public String getModelo() {
 		return modelo;
-	}
-	private void setModelo(String modelo) {
-		this.modelo = modelo;
 	}
 	public int getAno() {
 		return ano;
 	}
-	private void setAno(int ano) {
-		this.ano = ano;
-	}
+
 	public float getPreco() {
 		return preco;
-	}
-	public void setPreco(float preco) {
-		this.preco = preco;
-	}
-
-	public void retirarVeiculo() {
-		quantidade--;
-	}
-	
-	public void adicionarVeiculo() {
-		quantidade++;
-	}
-	
-	public void aumentarPreco() {
-		preco++;
-	}
-	
-	public void diminuirPreco() {
-		preco--;
 	}
 }
